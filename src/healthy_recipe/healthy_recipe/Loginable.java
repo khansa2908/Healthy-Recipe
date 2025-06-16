6470b8090b6bb5827abae31ff700c9b9d1663c77
tiara salsabila <tiara salsabila@LAPTOP-C66F2O9D>
2025-06-16 19:45:09 +0700

    First commit

---
+++ src/healthy_recipe/healthy_recipe/Loginable.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package healthy_recipe.healthy_recipe;
+
+/**
+ *
+ * @author tiara salsabila
+ */
+public interface Loginable {
+    boolean login(String username, String password, String role);
+}
